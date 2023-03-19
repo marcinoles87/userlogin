27ea826bcb77908ac67e4adc6a8f0b9bc742510f
marcinoles87 <marcinoles87@gmail.com>
2023-03-19 20:56:14 +0100

    work with autho

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Auth0Provider} from '@auth0/auth0-react'
+
+ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+ const client = process.env.REACT_APP_AUTH0_CLIENT;
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider>
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
