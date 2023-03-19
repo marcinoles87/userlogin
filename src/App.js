b42862c60c4997b55603acb9abcc4c203fdd092b
marcinoles87 <marcinoles87@gmail.com>
2023-03-19 21:03:55 +0100

    work with autho

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import { LoginButton } from './components/LoginButton';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginButton></LoginButton>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import App from './App';
 import {Auth0Provider} from '@auth0/auth0-react'
 
  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
- const client = process.env.REACT_APP_AUTH0_CLIENT;
+ const clientId = process.env.REACT_APP_AUTH0_CLIENT;
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider>
+  <Auth0Provider
+  domain={domain}
+  clientId={clientId}
+  redirectUri={window.location.origin}
+  
+  >
+
     <App />
   </Auth0Provider>
 );
